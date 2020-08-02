cd8237e5788f131d2b66f62e1baeb4fe0ec4aadd
drehtisch <38186302+drehtisch@users.noreply.github.com>
2020-08-02 03:36:33 +0200

    Vote Validation

--- Office.cs
+++ Office.cs
@@ -12,7 +12,8 @@ namespace IRV_Evaluator
         public List<Vote> Votes { get; set; } = new List<Vote>();
         public int ValidVoteCount => Votes.Count(v => v.IsValid);
         public int StartIndex { get; set; }
-
+        public bool DoValidation { get; set; }
+        public int ValidationColumnIndex { get; set; }
         public void LoadCandidates()
         {
             foreach (Candidate candidate in Candidates)
@@ -25,19 +26,47 @@ namespace IRV_Evaluator
         }
         public void ReadVote(GenericParser parser)
         {
-            var vote = new Vote() { IsValid = true };
+            var vote = new Vote();
+            ValidateVote(parser, vote);
             for (int i = StartIndex; i < StartIndex + Candidates.Count; i++)
             {
-                var parsed = int.TryParse(parser[i], out int parsedValue);
-                if (!parsed)
-                {
-                    parsedValue = -1;
-                    Console.WriteLine($"Value couldn't be parsed. Row: {parser.DataRowNumber} Column: {i}");
-                }
-                vote.Rankings.Add(parsedValue);
+                ParseAndAddVote(parser, vote, i);
             }
             Votes.Add(vote);
         }
+
+        private void ValidateVote(GenericParser parser, Vote vote)
+        {
+            if (DoValidation)
+            {
+                var validParsed = int.TryParse(parser[ValidationColumnIndex], out int isValid);
+                if (!validParsed)
+                {
+                    vote.IsValid = false;
+                    Console.WriteLine($"Value couldn't be parsed. Row: {parser.DataRowNumber} Column: {ValidationColumnIndex}");
+                }
+                else
+                {
+                    vote.IsValid = Convert.ToBoolean(isValid);
+                }
+            }
+            else
+            {
+                vote.IsValid = true;
+            }
+        }
+
+        private static void ParseAndAddVote(GenericParser parser, Vote vote, int i)
+        {
+            var val = parser[i];
+            var parsed = int.TryParse(val, out int parsedValue);
+            if (!parsed)
+            {
+                Console.WriteLine($"Value couldn't be parsed. Row: {parser.DataRowNumber} Column: {i}");
+            }
+            vote.Rankings.Add(parsedValue);
+        }
+
         public void RecalculatePercentage()
         {
             Candidates.ForEach(c => c.Percentage = c.VoteCount * 100.00 / ValidVoteCount);
