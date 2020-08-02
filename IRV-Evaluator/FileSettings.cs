f4a97505943461a7fd2c4fae542e1382bec2e047
drehtisch <38186302+drehtisch@users.noreply.github.com>
2020-08-02 15:16:10 +0200

    Read from Config & Reorganized Structure

---
+++ IRV-Evaluator/FileSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace IRV_Evaluator
+{
+    class FileSettings
+    {
+        public string Path { get; set; }
+        public char Delimiter { get; set; }
+        public bool FirstRowHasHeader { get; set; }
+    }
+}
---
+++ IRV-Evaluator/Program.cs
@@ -0,0 +1,50 @@
+﻿using GenericParsing;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace IRV_Evaluator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetParent(AppContext.BaseDirectory).FullName)
+                .AddJsonFile("appsettings.json", false)
+                .Build();
+
+                var fileSettings = configuration.GetSection("FileSettings").Get<FileSettings>();
+                var offices = configuration.GetSection("Offices").Get<IEnumerable<Office>>();
+                using (var parser = new GenericParser(fileSettings.Path))
+                {
+                    parser.FirstRowHasHeader = fileSettings.FirstRowHasHeader;
+                    parser.ColumnDelimiter = fileSettings.Delimiter;
+                    parser.FirstRowSetsExpectedColumnCount = true;
+                    parser.TrimResults = true;
+                    while (parser.Read())
+                    {
+                        foreach (var office in offices)
+                        {
+                            office.ReadVote(parser);
+                        }
+                    }
+                }
+                foreach (var office in offices)
+                {
+                    Evaluator.Evaluate(office);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"A fatal error occurred.{Environment.NewLine}{ex}");
+            }
+            Console.WriteLine("Evaluation complete");
+            Console.ReadKey();
+        }
+    }
+}
